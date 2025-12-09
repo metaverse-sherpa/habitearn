4365e685946413978ad1296d3fa142bebea98b3e
metaverse-sherpa <120398113+metaverse-sherpa@users.noreply.github.com>
2025-12-09 09:28:21 +0000

    Fixed the form so it shows a confirmation

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse
 
 from contact_us.forms import ContactUsForm
 
@@ -11,18 +12,22 @@ def home(request):
         print(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'successfully submitted!')
-            return redirect('home:home')
+            messages.success(request, 'Thanks for joining the waitlist!')
+            request.session['waitlist_success'] = True
+            return redirect(f"{reverse('home:home')}#wait")
         else:
             print(form.errors)
             if 'email' in form.errors:
                 messages.error(request, 'Invalid email address. Please correct it.')
-                return redirect('home:home')
+                return redirect(f"{reverse('home:home')}#wait")
             else:
                 messages.error(request, 'Please correct the errors in the form.')
-                return redirect('home:home')
+                return redirect(f"{reverse('home:home')}#wait")
+
+    waitlist_success = request.session.pop('waitlist_success', False)
 
     context = {
         "form": form,
+        "waitlist_success": waitlist_success,
     }
     return render(request, 'home/home.html', context)
